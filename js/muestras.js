02b97bf6b18493602ee061ccdb1c1a4ac353940f
victor-pc <vico.16c@gmail.com>
2017-10-20 03:18:28 -0500

    add contenido

---
+++ js/muestras.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+
+		var res = tab_id.split("-");
+		//console.log("#"+tab_id+' #slides'+res[1]+' .slidesjs-container');
+		$("#"+tab_id+' #slides'+res[1]+' .slidesjs-container').css("width", "836");
+		$("#"+tab_id+' #slides'+res[1]+' .slidesjs-container').css("height", "469.583");
+	})
+
+})
